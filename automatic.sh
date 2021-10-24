856e6e7489c4bdb09cfa6fb155da96c805fa9a9e
Jurriaan Den Toonder <1493561+Fastjur@users.noreply.github.com>
2021-10-24 23:20:14 +0200

    Add back correct automatic.sh script

--- automatic.sh
+++ automatic.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-# declare -a dataParalellisms=("4" "3" "2" "1")
-# declare -a executorCores=("1250m" "1125m" "1000m")
-# declare -a learningRates=("0.01" "0.02" "0.04")
-declare -a dataParalellisms=("4")
-declare -a executorCores=("1250m")
-declare -a learningRates=("0.01")
-declare -a batchSizes=("16" "32" "48" "64" "80" "96" "112" "128" "144" "160" "176" "192" "208" "224" "240" "256" "272" "288" "304" "320" "336" "352" "368" "384" "400" "416" "432" "448" "464" "480" "496" "512" "528" "544" "560" "576" "592" "608" "624" "640" "656" "672" "688" "704" "720" "736" "752" "768" "784" "800" "816" "832" "848" "864" "880" "896" "912" "928" "944" "960" "976" "992" "1008" "1024")
+declare -a dataParalellisms=("4" "3" "2" "1")
+declare -a executorCores=("1250m" "1125m" "1000m")
+declare -a learningRates=("0.01" "0.02" "0.04")
+declare -a batchSizes=("128" "256" "512")
 
 for dataParallelism in ${dataParalellisms[@]]}; do
 	for executorCore in ${executorCores[@]}; do
